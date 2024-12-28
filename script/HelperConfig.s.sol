73962cb40c4bcf033b4500ad5fa8a135a1272fdf
nicktwist21 <nickfc15@gmail.com>
2024-12-28 03:10:20 -0500

    MockV3Aggregator.sol is added to the project

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        // 1. Deploy Mocks
+        // 2. Return Mock Address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed) // replace with actual price feed address
+        });
+        return anvilConfig;
+    }
+}
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,14 +4,17 @@ pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
-        
+
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
@@ -19,11 +22,16 @@ contract FundMeTest is Test {
     function testOwnerIsMsgSender() public {
         console.log(fundMe.i_owner());
         console.log(msg.sender);
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public {
-        uint256 version = fundMe.getVersion();
-        assertEq(version, 4);
+        if (block.chainid == 11155111) {
+            uint256 version = fundMe.getVersion();
+            assertEq(version, 4);
+        } else if (block.chainid == 1) {
+            uint256 version = fundMe.getVersion();
+            assertEq(version, 6);
+        }
     }
 }
